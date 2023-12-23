a99b13388fee989a441fb573b8629def7bdd914d
Az Foxxo <knotgamestudio@gmail.com>
2023-12-23 14:15:07 +0000

    Created loader for Virtual Machine and VM class

--- FoxVision/src/Program.cs
+++ FoxVision/src/Program.cs
@@ -1,2 +1,110 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+using Fox16Shared;
+
+namespace FoxVision
+{
+    public class Program()
+    {
+        private static VirtualMachine? vm;
+
+        public static void Main()
+        {
+            var RawROM = LoadROMFile("fox16.bin");
+
+            // Print the first 10 bytes of the ROM as ASCII
+            Console.Write("10 byte identifier: ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write((char)RawROM[i]);
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+
+            // Delete the first 10 bytes of the ROM
+            RawROM = RawROM.Skip(10).ToArray();
+
+            // Convert the ROM to a ushort array (reversing array order to factor in big-endian)
+            ushort[] ROM = new ushort[RawROM.Length / 2];
+            for (int i = 0; i < ROM.Length; i++)
+            {
+                if (BitConverter.IsLittleEndian)
+                {
+                    Array.Reverse(RawROM, i * 2, 2);
+                }
+                ROM[i] = BitConverter.ToUInt16(RawROM, i * 2);
+            }
+
+            DebugLogROMAsData(ROM);
+
+            vm = new VirtualMachine(ROM);
+        }
+
+        /// <summary>
+        /// Load the ROM file
+        /// </summary>
+        /// <param name="ROMfile">The ROM file to load</param>
+        /// <returns>The ROM file as a byte array</returns>
+        private static byte[] LoadROMFile(string ROMfile) => File.ReadAllBytes(ROMfile);
+
+        private static void DebugLogROMAsData(ushort[] ROM)
+        {
+            Console.WriteLine("ROM contents: ");
+            for (var i = 0; i < ROM.Length; i++)
+            {
+                // Look up the opcode corresponding to the value
+                var opcodes = Opcodes.GetKey(ROM[i]);
+
+                // Convert string array to string with spaces
+                string opcode = string.Join(" ", opcodes);
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write($"${i:X4}");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write($" {ROM[i]:X2}");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write($"→");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write($"{opcode} ");
+
+                // If instruction takes address or value, skip
+                Console.ForegroundColor = ConsoleColor.Green;
+                switch (opcode)
+                {
+                    case "LFM":
+                    case "WTM":
+                    case "JNZ":
+                    case "JMP":
+                    case "JPZ":
+                    case "PPU_LFM":
+                    case "PPU_WTV":
+                    case "PPU_AND":
+                    case "PPU_XOR":
+                    case "PPU_ORA":
+                        // Convert byte array to ushort
+                        Console.Write($"{ROM[i + 1]:x4} ({ROM[i + 1]})");
+                        i += 1;
+
+                        break;
+
+                    case "SRA":
+                    case "PPU_DRW":
+                    case "DWR":
+                        // Convert byte array to ushort
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.Write($"({ROM[i + 1]})");
+                        i += 1;
+                        break;
+
+                    default:
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        Console.Write("NOR");
+                        break;
+                }
+
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+    }
+}
---
+++ FoxVision/src/VirtualMachine.cs
@@ -0,0 +1,39 @@
+using FoxVision.Components;
+
+namespace FoxVision
+{
+    internal class VirtualMachine
+    {
+        private ContiguousMemory _unprotectedMemory;
+        private Processor _processor;
+        internal VirtualMachine(ushort[] ROM)
+        {
+            // Create a new block of contiguous memory for the RAM
+            _unprotectedMemory = new(ushort.MaxValue);
+
+            // Copy the first 4kb of ROM into the unprotected memory or all room, whichever is smaller
+            var size = Math.Min(ROM.Length, 4096);
+
+            // In case of odd number, add a zero to the end of the ROM
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            for (ushort i = 0; i < size; i++)
+            {
+                _unprotectedMemory.WriteUnchecked(i, ROM[i]);
+                Console.Write(_unprotectedMemory.ReadUnchecked(i).ToString("X4") + " ");
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+
+            Console.WriteLine("ROM copied to RAM");
+
+            // Create the CPU
+            _processor = new(_unprotectedMemory);
+            
+            // Enter CPU cycle
+            while (!_processor.ExecuteCycle())
+            {
+                
+            }
+        }
+    }
+}
